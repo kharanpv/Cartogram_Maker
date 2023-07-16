9c662bb2c0e835f427a53eea902c02dd91b47847
Cat Luong <luongcat162002@gmail.com>
2023-07-16 03:26:24 -0400

    new changes -  add plus minus button, make some new placeholders, change the title of the app, will continue to work on dragging around the canvas

--- cartogram_maker/test_canvas.py
+++ cartogram_maker/test_canvas.py
@@ -28,13 +28,16 @@ def create_click_event(canvas):
 
     return click_event
 
-def on_plus_click(): #placeholder for future Plus button functionality
-    
+def on_plus_click(canvas): #placeholder for future Plus button functionality
+    canvas.scale("all", 0, 0, 1.1, 1.1)     # Scale by 10%
     print("[DEBUG]: Plus button pressed")
 
-def on_minus_click(): #placeholder for future Minus button functionality
-
+def on_minus_click(canvas): #placeholder for future Minus button functionality
+    canvas.scale("all", 0, 0, 0.9, 0.9)
     print("[DEBUG]: Minus button pressed")
+
+def on_button_release(event): #placeholder for future Left mouse button functionality
+    print("[DEBUG]: Left mouse button released")
 
 def generate(): #placeholder for future Generate button functionality
     
@@ -44,8 +47,17 @@ def go_back(): #placeholder for future Go Back button functionality
 
     print("[DEBUG]: Go Back button pressed")
 
+def start_drag(event, canvas): # Place holder for dragging around functionality
+
+    print("[DEBUG]: ")
+
+def drag(event, canvas):
+
+    print("[DEBUG]: ")
+
 def main():
     app = ctk.CTk()  # Create the application window
+    app.title("Cartogram Maker") # Add title to this app
     app.geometry("800x600")  # Set the window size
     app.grid_columnconfigure(0, weight=1)
     app.grid_rowconfigure(0, weight=1)
@@ -65,6 +77,7 @@ def main():
     canvas.grid(row=0, column=0, padx=0, pady=0, sticky="nsew")  # Add padding for the canvas
 
     canvas.bind("<Button-1>", create_click_event(canvas=canvas))
+    # button.bind("<ButtonRelease-1>", create_)
 
     print("[DEBUG]: created canvas and bound on click event")
 
@@ -78,14 +91,15 @@ def main():
 
     print("[DEBUG]: created Go Back button")
 
-    plus_button = ctk.CTkButton(frame, text="+", command=on_plus_click, width=40, fg_color="lightgray", text_color="black",
-                                 font=("Arial", 30), corner_radius=0, hover_color="lightgray")
+    plus_button = ctk.CTkButton(frame, text="+", command=lambda: on_plus_click(canvas), width=40, fg_color="lightgray", text_color="black",
+                                 font=("Arial", 30), corner_radius=0, hover_color="darkgray")
     plus_button.grid(row=0, column=1, padx=(0, 5), pady=(0,50), sticky="se")
 
-    minus_button = ctk.CTkButton(frame, text="-", command=on_minus_click, width=40, fg_color="lightgray", text_color="black",
-                                 font=("Arial", 30), corner_radius=0, hover_color="lightgray")
+    minus_button = ctk.CTkButton(frame, text="-", command=lambda: on_plus_click(canvas), width=40, fg_color="lightgray", text_color="black",
+                                 font=("Arial", 30), corner_radius=0, hover_color="darkgray")
     minus_button.grid(row=0, column=1, padx=(0,5), pady=(0,5), sticky="se")
 
     print("[DEBUG]: created Plus and Minus buttons")
+
 
     app.mainloop()  # Start the application's main event loop
